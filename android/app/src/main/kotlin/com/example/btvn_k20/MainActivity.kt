e84a01761098c1f4b3751eb96887777f0ca31c16
vietkt <kieutuanviet@gmail.com>
2024-07-28 09:28:39 +0700

    aaaa

---
+++ android/app/src/main/kotlin/com/example/btvn_k20/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.btvn_k20
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // Here is main function
+  // Put your "calling" functions here (ex: Function ex1(), Function ex2())
+  // Create a logic file (ex: logic.dart) your self and put it in lib/logic folder
+  // Create a model file (ex: person.dart) your self and put it in lib/model folder
+}
